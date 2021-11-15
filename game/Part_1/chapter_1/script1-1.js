5fe9f8882e26718f306fee123f0f0c0ad1ca8242
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2021-11-15 17:35:32 +0100

    Callback hell but working ^^

--- game/Part_1/chapter_1/script1-1.js
+++ game/Part_1/chapter_1/script1-1.js
@@ -1,3 +1,43 @@
-letterByLetter("Ho.. Tu es finalement réveillé ? ");
 whoIsTalking("???");
+letterByLetter("Ho.. Tu es finalement réveillé ? ");
 userOptions(">");
+
+// name, message, opt1, opt2, opt3, opt4
+document.getElementById(">").addEventListener("click", () => {
+	dialogActualisation("???", "Comment tu te sens?", ">");
+	document.getElementById(">").addEventListener("click", () => {
+		dialogActualisation("???", "Tu nous as fait peur...", ">");
+		document.getElementById(">").addEventListener("click", () => {
+			dialogActualisation("chevrette", "Je suis Chevrette, la fée de Saint-Rémy, comment t’appelles tu ?");
+			displayChevrette();
+
+			document.getElementById("userOptions").innerHTML = userNameInput;
+			document.getElementById("userNameSubmit").addEventListener("click", () => {
+				nameValidator();
+				document.getElementById(">").addEventListener("click", () => {
+					dialogActualisation("chevrette", "yolo", ">");
+				});
+			});
+		});
+	});
+});
+
+userNameInput = `
+<div id ="userInput" class="nes-field is-inline userInput">
+    <label for="dark_field" style="color:#fff;">Nom d'aventurier</label>
+    <input id="userInputValue" type="text" id="dark_field" class="nes-input is-dark" placeholder="Denis">
+    <button id="userNameSubmit" class="nes-btn" href="#">
+        ok
+    </button>
+</div>`;
+
+function nameValidator() {
+	let userInput = document.getElementById("userInputValue");
+	if (userInput.value == "") {
+		alert("Il faut choisir un nom :o");
+	} else {
+		userName = userInput.value;
+		localStorage.setItem("userName", userName);
+		dialogActualisation("Chevrette", `Très bien ${userName}, je pense que tu es arrivé ici pour nous apporter ton aide.`, ">");
+	}
+}
--- public/js/dialog.js
+++ public/js/dialog.js
@@ -1,3 +1,7 @@
+function whoIsTalking(name) {
+	document.getElementById("caracterName").innerHTML = name;
+}
+
 function letterByLetter(message) {
 	let i = 0;
 	let interval = setInterval(function () {
@@ -6,25 +10,38 @@ function letterByLetter(message) {
 		if (i > message.length) {
 			clearInterval(interval);
 		}
-	}, 50);
+	}, 10);
 }
 
-function whoIsTalking(name) {
-	document.getElementById("caracterName").innerHTML = name;
-}
-
-function userOptions(opt1, opt2 = "", opt3 = "", opt4 = "") {
+function userOptions(opt1 = "", opt2 = "", opt3 = "", opt4 = "") {
 	let userOptions = document.getElementById("userOptions");
-	userOptions.innerHTML += `<div class="nes-btn options">${opt1}</div>`;
+	userOptions.innerHTML += `<div id="${opt1}" class="nes-btn options">${opt1}</div>`;
 	if (opt2 != "") {
-		userOptions.innerHTML += `<div class="nes-btn options">${opt2}</div>`;
+		userOptions.innerHTML += `<div id="${opt2}" class="nes-btn options">${opt2}</div>`;
 	}
 	if (opt3 != "") {
-		userOptions.innerHTML += `<div class="nes-btn options">${opt3}</div>`;
+		userOptions.innerHTML += `<div id="${opt3}" class="nes-btn options">${opt3}</div>`;
 	}
 	if (opt4 != "") {
-		userOptions.innerHTML += `<div class="nes-btn options">${opt4}</div>`;
+		userOptions.innerHTML += `<div id="${opt4}" class="nes-btn options">${opt4}</div>`;
 	}
 }
 
-function dialogActualisation(message, name, a, b, c, d)
+function dialogActualisation(name, message, a, b, c, d) {
+	let cacarcterName = document.getElementById("caracterName");
+	let caracterSpeech = document.getElementById("caracterDialog");
+	let BtnContainer = document.getElementById("userOptions");
+
+	cacarcterName.innerHTML = "";
+	caracterSpeech.innerHTML = "";
+	BtnContainer.innerHTML = "";
+
+	cacarcterName = whoIsTalking(name);
+	BtnContainer = userOptions(a, b, c, d);
+	caracterSpeech = letterByLetter(message);
+}
+
+function displayChevrette() {
+	let chevrette = document.getElementById("caracterOne");
+	chevrette.innerHTML = `<img src="../../../public/assets/img/chevrette.gif" alt="" />`;
+}
