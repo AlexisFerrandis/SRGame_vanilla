88086a6d9c0d10a341abd790ecae71f476c3fa54
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2021-11-19 14:00:54 +0100

    Eleventh stage complete

--- game/Part_1/chapter_11/script1-11.js
+++ game/Part_1/chapter_11/script1-11.js
@@ -1,2 +1,6 @@
 refreshUserLife(0);
 storyTextDisplay("VOUS AVEZ VOS CARNETS JEUNES GENS ???");
+opponentMovment("defrancoformed.gif");
+slashBattleUserLife();
+refreshDialogOpponent("CHEWWWIIIINNGGGGGUUUUMMMSSS !!!", "LES CHAISES ONT QUATRE PIEDS !!!", "HAHAHAHHAAHAHA !!!");
+playChapterMusic(`stage1-11.mp3`);
---
+++ game/Part_1/chapter_12/script1-12.js
@@ -0,0 +1,11 @@
+displayThePet();
+refreshUserLife(0);
+thisIsMySword("divinilame.gif");
+displayChevrette();
+displayInterlocutor("defrancoformed.gif");
+
+playChapterMusic(`stage1-12.mp3`);
+
+whoIsTalking("Chevrette");
+letterByLetter("Pas très accueillant comme endroit !");
+userOptions(">");
